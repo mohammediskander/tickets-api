fda9012dfa4eb6ef063bbc9d7d2691da47108b6f
Mohammed Eskander <idarkcry@icloud.com>
2020-10-31 00:10:08 +0300

    Add ticket/create

---
+++ src/app/routes/ticket/create.ts
@@ -0,0 +1,36 @@
+import {Request, Response, NextFunction} from "express";
+import { Ticket, TicketInterface } from "../../models"
+
+export default (request: any, response: Response, next: NextFunction) => {
+  new Ticket({
+    name: request.body.name,
+    user: request.user._id,
+    description: request.body.description,
+    details: {
+      type: request.body.details.type,
+      expectedDeliveryDate: request.body.details.expectedDeliveryDate,
+      driver: {
+        car: {
+          model: request.body.details.driver.car.model,
+          licencePlate: request.body.details.driver.car.licencePlate,
+          color: request.body.details.driver.car.color
+        },
+        name: request.body.details.driver.name,
+      }
+    },
+    status: request.body.status
+  })
+    .save()
+    .then((ticket: TicketInterface) => {
+      if (!ticket) {
+        response.status(400)
+        next(new Error("ticketTreationError"))
+      } else {
+        response.json(ticket)
+      }
+    })
+    .catch(error => {
+      response.status(404)
+      next(error)
+    })
+}
